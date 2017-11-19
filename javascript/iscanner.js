9664ad03372f68300ed912ad7356e4a585759614
xudafeng <xudafeng@126.com>
2017-11-19 10:43:23 +0800

    update

--- javascript/iscanner.js
+++ javascript/iscanner.js
@@ -1,10 +1,10 @@
 // iscanner demo
 'use strict';
 
-+function(global, undefined) {
+(function(global, undefined) {
   var api = './api/latest.json';
   var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", api, true);
+  xmlHttp.open('GET', api, true);
   xmlHttp.onreadystatechange = function(d) {
     if (xmlHttp.readyState == 4) {
       callback(xmlHttp.responseText);
@@ -13,12 +13,12 @@
   xmlHttp.send(null);
   var callback = function(data) {
     data = JSON.parse(data);
-    document.getElementById('ios_version').innerHTML = data.ios.version;
-    document.getElementById('android_version').innerHTML = data.android.version;
+    document.querySelector('#ios_version').innerHTML = data.ios.version;
+    document.querySelector('#android_version').innerHTML = data.android.version;
   };
   var ua = navigator.userAgent.toLowerCase();
 
   if (!!~ua.indexOf('micromessenger')) {
-    document.getElementById('unsupport').style.display = 'block';
+    document.querySelector('#unsupport').style.display = 'block';
   }
-}(this);
+})(this);
